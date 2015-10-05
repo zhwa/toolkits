57457337e9a0c747a60d1baeb674b01bcf141657
Zhe Wang <wangzhe0543@gmail.com>
2015-10-04 20:30:06 -0400

    bug fix

--- algorithms/tree.py
+++ algorithms/tree.py
@@ -6,13 +6,16 @@ Z. Wang
 wangzhe0543@gmail.com
 """
 class PriorityQueue(object):
+    """
+    data format: (key, value) pair
+    """
     def __init__(self):
         self.heap = [(0,0)]
         self.size = 0
 
     def build(self, rawList):
         self.size = len(rawList)
-        self.heap = [(0,0)]
+        self.heap = [(0,None)]
         for element in rawList:
             self.heap.append(element)
         mid = self.size // 2
@@ -23,7 +26,7 @@ class PriorityQueue(object):
     def percDown(self, idx):
         while idx * 2 <= self.size:
             mChild = self.minChild(idx)
-            if self.heap[idx][0] > self.heap[mChild][0]:
+            if self.heap[idx][1] > self.heap[mChild][1]:
                 self.heap[idx], self.heap[mChild] = self.heap[mChild], self.heap[idx]
             idx = mChild
 
@@ -34,14 +37,14 @@ class PriorityQueue(object):
             if idx * 2 + 1 > self.size:
                 return idx * 2
             else:
-                if self.heap[idx*2][0] < self.heap[idx*2+1][0]:
+                if self.heap[idx*2][1] < self.heap[idx*2+1][1]:
                     return idx * 2
                 else:
                     return idx * 2 + 1
 
     def percUp(self, idx):
         while idx // 2 > 0:
-            if self.heap[idx][0] < self.heap[idx//2][0]:
+            if self.heap[idx][1] < self.heap[idx//2][1]:
                 self.heap[idx], self.heap[idx//2] = self.heap[idx//2], self.heap[idx]
             idx = idx // 2
 
@@ -51,7 +54,7 @@ class PriorityQueue(object):
         self.percUp(self.size)
 
     def delMin(self):
-        retval = self.heap[1][1]
+        retval = self.heap[1][0]
         self.heap[1] = self.heap[self.size]
         self.heap.pop()
         self.percDown(1)
@@ -61,14 +64,14 @@ class PriorityQueue(object):
         return self.size == 0
 
     def modifyKey(self, old, new):
-        idx = 1
+        idx = 0
         while idx <= self.size:
             if selfheap[idx][1] == old:
                 break
             else:
                 idx += 1
         if idx > 0:
-            self.heap[idx] = (new, self.heap[idx][1])
+            self.heap[idx] = (self.heap[idx][0], new)
             if new < old:
                 self.percUp(idx)
             else:
