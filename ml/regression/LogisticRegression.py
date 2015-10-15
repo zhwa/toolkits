4a71bf798eebbcede6550758f9ccbf74fd617cd0
Zhe Wang <wangzhe0543@gmail.com>
2015-10-14 21:55:54 -0400

    delete two class LR

--- ml/regression/LogisticRegression.py
+++ ml/regression/LogisticRegression.py
@@ -11,9 +11,7 @@ Z. Wang
 wangzhe0543@gmail.com
 """
 from __future__ import division
-from operator import add
 import numpy as np
-from ..regression import bfgs
 
 
 
@@ -31,7 +29,9 @@ class LR(object):
         return prob.index(max(prob))
 
     def train(self, data, labels):
-        """ data should be in d dimension """
+        """
+        Gradient Decent
+        """
         step = 100
         alpha = 0.01
         iters = 0
@@ -42,49 +42,3 @@ class LR(object):
                 self.beta[k,:] += alpha * step
 
 
-    
-
-
-
-
-
-# The sigmoid function
-sigmoid = lambda z: 1.0 / (1.0 + np.e ** (-1.0 * z))
-
-
-# gradient function
-def g(w, x, y):
-    """
-    x: n * d, each rwo corresponds to one sample
-    y: n * 1, lable
-    w: 1 * d
-    """
-    yx = np.array(map(lambda vec: vec[:-1] * vec[-1], np.hstack((x,y.reshape((-1,1))))))
-    gradient = reduce(add, map(lambda vec: (sigmoid(w.dot(vec)) - 1) * vec, yx))
-    return gradient
-
-
-def train(labeledData, alpha):
-    raws = labeledData[:,:-1]
-    labels = labeledData[:,-1]
-    samples = np.hstack(np.ones((-1, raw.shape[0])), raws)
-
-    # loop
-    w = np.random.rand(samples.shape[1])
-    for i in range(100):
-        w = w - alpha * g(w, samples, labels)
-
-    return w
-
-
-def predict(rawVec, w):
-    vec = np.hstack((1, rawVec))
-    score = sigmoid(w.dot(vec))
-    if score > 0.5:
-        return 1
-    else:
-        return 0
-
-
-
-    
