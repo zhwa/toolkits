96c512aee479bbd4c2de41e99111a5bc709fbcf6
zhwa <wangzhe0543@126.com>
2015-10-26 19:36:18 -0400

    entropy calculation

--- ml/tree/utils.py
+++ ml/tree/utils.py
@@ -29,3 +29,18 @@ def ent(dataSet):
         prob = cates[label] / N
         ent -= prob * np.log2(prob)
     return ent
+
+
+def infoGain(dataSet1, dataSet2):
+    """
+    information gain after the split
+    """
+    ent1 = ent(dataSet1)
+    N1 = len(dataSet1)
+    ent2 = ent(dataSet2)
+    N2 = len(dataSet2)
+    newEnt = (N1 / (N1 + N2)) * ent1 + (N2 / (N1 + N2)) * ent2
+    oldEnt = ent(np.vstack((dataSet1, dataSet2)))
+    return oldEnt - newEnt
+
+
