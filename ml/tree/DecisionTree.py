de3c90dba5df48c94194ced149695bf9e0c1db3f
Zhe Wang <wangzhe0543@gmail.com>
2016-02-02 21:03:53 -0500

    decision tree

--- ml/tree/DecisionTree.py
+++ ml/tree/DecisionTree.py
@@ -5,55 +5,53 @@ Naive Decesion Tree
 from __future__ import division
 import numpy as np
 import operator
-from utils import ent, createDataSet
+from utils import sampleData, entropy, split, stats
+
 
 
 class node(object):
-    def __init__(self, key, func, level, data):
-        self.key = key
-        self.func = func
-        self.level = level
-        self.data = data
-        self.next = []
-
-    def _classify(self):
-        labels = {}
-        for e in self.data:
-            if e[-1] in labels:
-                labels[e[-1]] += 1
-            else:
-                labels[e[-1]] = 1
-        return max(labels.keys(), key=(lambda k: labels[k]))
-
-    def classify(self, vec):
-        if self.next == []:
-            return _classify()
-        else:
-            check = [n.func(vec) for n in self.next]
-            return self.next[check.index(True)].classify(vec)
+    """
+    Arguments:
+        feature: the index of the column in the data set
+        threshold: boundary for binary split
+        results: dict to store the statics of category corresponding to the current node
+        left: child node for the data item when feature value < threshold
+        right: child node
+    """
+    def __init__(slef, feature=0, threshold=None, results=None, left=None, right=None):
+        self.feature = feature
+        self.threshold = threshold
+        self.results = results
+        self.left = left
+        self.right = right
 
 
-    def train(self, mxLevel):
-        if self.level >= mxLevel:
-            self.next = []
-        else:
-            """ create children nodes """
-            pass
+def buildTree(data):
+    if len(data) <= 0: return node()
 
-    
-        
+    currentEnt = entropy(data)
+    bestGain = 0.0
+    bestCriteria = None
+    bestSets = None
 
-    
+    dimension = len(data[0]) - 1
 
+    for feature in range(dimension):
+        feature_values = {}
+        for item in data:
+            feature_values[data[feature]] = 1
+        for value in feature_values.keys():
+            set1, set2 = split(data, feature, value)
+            p = len(set1) / len(set2)
+            infoGain = currentEnt - p * entropy(set1) - (1 - p) * entropy(set2)
+            if infoGain > bestGain and len(set1) > 0 and len(set2) > 0:
+                bestGain = infoGain
+                bestCriteria = (feature, value)
+                bestSets = (set1, set2)
 
-class tree(object):
-    def __init__(self, data, depth):
-        self.data = data
-        self.root = node(0, None, None)
-        self.depth = depth
-
-    def train(self):
-        """ BFS for training """
-        pass
-
-    
+    if bestGain > 0:
+        leftBranch = buildTree(bestSet[0])
+        rightBranch = buildTree(bestSet[1])
+        return node(feature=bestCriteria[0], threshold=bestCriteria[1], left=leftBranch, right=rightBranch)
+    else:
+        return node(results=stats(data))
--- ml/tree/utils.py
+++ ml/tree/utils.py
@@ -8,34 +8,64 @@ wangzhe0543@gmail.com
 from __future__ import division
 import numpy as np
 
+def simpleData():
+    data = [['slashdot','USA','yes',18,'None'],
+        ['google','France','yes',23,'Premium'],
+        ['digg','USA','yes',24,'Basic'],
+        ['kiwitobes','France','yes',23,'Basic'],
+        ['google','UK','no',21,'Premium'],
+        ['(direct)','New Zealand','no',12,'None'],
+        ['(direct)','UK','no',21,'Basic'],
+        ['google','USA','no',24,'Premium'],
+        ['slashdot','France','yes',19,'None'],
+        ['digg','USA','no',18,'None'],
+        ['google','UK','no',18,'None'],
+        ['kiwitobes','UK','no',19,'None'],
+        ['digg','New Zealand','yes',12,'Basic'],
+        ['slashdot','UK','no',21,'None'],
+        ['google','UK','yes',18,'Basic'],
+        ['kiwitobes','France','yes',19,'Basic']]
+    return data
 
-def ent(dataSet):
-    """
-    calculate the Shannon Enropy of a given data set
-    Arguments:
-        dataSet: labeled data, the last volume should be the labels
-        return: entropy in real values
-    """
-    N = len(dataSet)
-    cates = {}
-    for vector in dataSet:
-        label = vector[-1]
-        if label in cates:
-            cates[label] += 1
+
+def split(data, feature, threshold):
+    if isinstance(threshold, str):
+        judge = lambda row: row[feature] == threshold
+    else:
+        judge = lambda row: row[feature] >= threshold
+    set1 = filter(judge, data)
+    set2 = filter(lambda row: row not in set1, data)
+    return set1, set2
+
+
+
+def stats(data):
+    cnt = {}
+    s = 0
+    for item in data:
+        s += 1
+        if item in cnt:
+            cnt[item] += 1
         else:
-            cates[label] = 1
-    ent = 0
-    for lable in cates:
-        prob = cates[label] / N
-        ent -= prob * np.log2(prob)
-    return ent
+            cnt[item] = 1
+    cnt['s'] = s
+    return cnt
 
 
-def createDataSet():
-    dataSet = [[1, 1, "yes"],
-               [1, 1, "yes"],
-               [1, 0, "no"],
-               [0, 1, "no"],
-               [0, 1, "no"]]
-    labels = ["no surfacing", "flippers"]
-    return np.array(dataSet), np.array(labels)
+def entropy(data):
+    categories = np.asarray(data)[:,-1]
+    cnt = stats(categories)
+    h = 0
+    N = cnt['s']
+    for item in cnt:
+        p = cnt[item] / N
+        h -= p * np.log2(p)
+    return h
+
+
+
+if __name__ == "__main__":
+    data = simpleData()
+    set1, set2 = split(data, 3, 20)
+    print entropy(set1)
+    print entropy(set2)
